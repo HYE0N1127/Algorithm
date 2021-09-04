6630f98624b03fff3d7f018d14c5ed2874f9e0bb
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-09-04 19:25:20 +0900

    ADD : Programmers Level 1 행렬의 덧셈

---
+++ Algorithm/Programmers/Level1/python/sum_procession.py
@@ -0,0 +1,8 @@
+# Programmers Level 1 - 행렬의 덧샘
+
+def solution(arr1, arr2):
+    for i in range(len(arr1)):
+        for j in range(len(arr1[i])):
+            arr1[i][j] += arr2[i][j]
+
+    return arr1
