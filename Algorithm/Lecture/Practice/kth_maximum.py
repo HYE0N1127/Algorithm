7242ea5d10d10253be7733d5dfe5b14e73141aa7
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2022-04-25 15:14:41 +0900

    ADD : K번째 큰 수

---
+++ Algorithm/Lecture/Practice/kth_maximum.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.stdin = open("input/kth_maximum/input.txt", "rt")
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+res = set()
+cnt = 0
+
+for i in range(n):
+    for j in range(i + 1, n):
+        for m in range(j + 1, n):
+            res.add(a[i] + a[j] + a[m])
+
+res = list(res)
+res.sort(reverse=True)
+print(res[k-1])
